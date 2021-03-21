d60e585e73b46113430112e557cdb2ca1606961f
Sarah <sarah.alhuriz@hotmail.com>
2021-03-21 00:45:05 -0600

    create upload.js and add multer storage

---
+++ api/middlewares/upload.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+
+const csvFilter = (req, file, cb) => {
+  if (file.mimetype.includes("csv")) {
+    cb(null, true);
+  } else {
+    cb("Only csv files are accepted", false);
+  }
+};
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __basedir + "/resources/static/assets/uploads/");
+  },
+  filename: (req, file, cb) => {
+    console.log(file.originalname);
+    cb(null, `${Date.now()}-gm-${file.originalname}`);
+  },
+});
+
+var uploadFile = multer({ storage: storage, fileFilter: csvFilter });
+
+
+module.exports = uploadFile;
