87a8c1efd12a4b4a2323011f04689b84b18a297d
Sarah <sarah.alhuriz@hotmail.com>
2021-03-22 20:58:30 -0600

    init Axios to send HTTP requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'; //compiled css
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
-class App extends Component {
-  render() {
-    
-  }
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default App;
