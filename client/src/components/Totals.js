0fa81a3321c9bf1faa36fbdefcccdcaff9a6ea47
Sarah <sarah.alhuriz@hotmail.com>
2021-03-23 20:14:52 -0600

    group items success

--- client/src/components/Totals.js
+++ client/src/components/Totals.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo, useRef } from "react";
 import ClientDataService from "../services/ClientService";
-import { useTable } from "react-table";
+import { useExpanded, useGroupBy, useTable } from "react-table";
 
 
 const Totals = (props) => {
@@ -23,50 +23,30 @@ const Totals = (props) => {
             });
     };
 
-    // const refreshList = () => {
-    //     retrieveClients();
-    // };
 
     const columns = useMemo(
         () => [
-            {
-                Header: 'Date',
-                accessor: 'date'
-            },
-            {
-                Header: 'Client',
-                accessor: 'client'
-            },
             {
                 Header: 'Project',
                 accessor: 'project'
             },
             {
-                Header: 'Project Code',
-                accessor: 'project_code'
+                Header: 'Client',
+                accessor: 'client',
+                aggregate: 'count',
+                Aggregated: ({ value }) => `${value} Unique Names`
             },
             {
-                Header: 'Hours',
-                accessor: 'hours'
+                Header: 'Billable Hours',
+                accessor: 'hours',
+                aggregate: 'sum',
+                Aggregated: ({ value }) => `${value} (total)`,
             },
             {
-                Header: 'Billable?',
-                accessor: 'billable',
-                Cell: (props) => {
-                    return props.value ? 'Yes' : 'No';
-                }
-            },
-            {
-                Header: 'First Name',
-                accessor: 'first_name'
-            },
-            {
-                Header: 'Last Name',
-                accessor: 'last_name'
-            },
-            {
-                Header: 'Billable Rate',
-                accessor: 'billable_rate'
+                Header: 'Billable Amount',
+                accessor: 'billable_rate',
+                aggregate: 'sum',
+                Aggregated: ({ value }) => `${value} (total)`,
             }
         ],
         []
@@ -78,14 +58,22 @@ const Totals = (props) => {
         headerGroups,
         rows,
         prepareRow,
-    } = useTable({
+        state: { groupBy, expanded },
+    } = useTable(
+        {
         columns,
-        data: clients,
-    });
+        data: clients
+        }, 
+        useGroupBy,
+        useExpanded
+    );
 
 
     return (
         <div className="col-md-12 list">
+            <pre>
+                <code>{JSON.stringify({ groupBy, expanded }, null, 2)}</code>
+            </pre>
             <table
             className="table table-striped table-bordered"
             {...getTableProps()}
@@ -95,6 +83,12 @@ const Totals = (props) => {
                     <tr {...headerGroup.getHeaderGroupProps()}>
                         {headerGroup.headers.map((column) => (
                         <th {...column.getHeaderProps()}>
+                            {column.canGroupBy ? (
+                                // If the column can be grouped, let's add a toggle
+                                <span {...column.getGroupByToggleProps()}>
+                                {column.isGrouped ? '🛑 ' : '👊 '}
+                                </span>
+                            ) : null}
                             {column.render("Header")}
                         </th>
                         ))}
@@ -108,7 +102,25 @@ const Totals = (props) => {
                         <tr {...row.getRowProps()}>
                         {row.cells.map((cell) => {
                             return (
-                            <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                            <td {...cell.getCellProps()}>
+                                {cell.isGrouped ? (
+                                    // If it's a grouped cell, add an expander and row count
+                                    <>
+                                        <span {...row.getToggleRowExpandedProps()}>
+                                            {row.isExpanded ? '👇' : '👉'}
+                                        </span>{' '}
+                                        {cell.render('Cell')} ({row.subRows.length})
+                                    </>
+                                ) : cell.isAggregated ? (
+                                    // If the cell is aggregated, use the Aggregated
+                                    // renderer for cell
+                                    cell.render('Aggregated')
+                                ) : cell.isPlaceholder ? null : ( // For cells with repeated values, render null
+                                    // Otherwise, just render the regular cell
+                                    // {cell.render("Cell")}
+                                    cell.render("Cell")
+                                )}
+                            </td>
                             );
                         })}
                         </tr>
